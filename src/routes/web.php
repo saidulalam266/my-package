805a4c1a2e427d914077a05901e929325e075873
saidul <saidulalam6012@gmail.com>
2021-02-28 17:01:52 +0600

    composer issue fixed2

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 use Saidul\Contact\Http\Controllers\ContactController;
 /////
 Route::get('/show-form', [ContactController::class, 'showForm']);
-Route::get('/send-mail', [ContactController::class, 'sendMail']);
+Route::post('/send-mail', [ContactController::class, 'sendMail']);
